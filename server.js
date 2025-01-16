d4f49d353e246eea155478426307708b75caf24e
Leandro Garcia <124641959+leandrogrc@users.noreply.github.com>
2025-01-16 01:38:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,4 @@ app.use(express.json());
 const { router } = require("./routes/userRoutes");
 app.use("/", router);
 
-//const { getUsers, getUser, newUser } = require("./database/database");
-//newUser("Carina", "carinabiabastos@gmail.com");
-//getUsers();
-//getUser(1);
-
 app.listen(PORT, () => console.log("Listening on port " + PORT));
